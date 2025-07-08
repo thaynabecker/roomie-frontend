95dc8fb7edd77c08fd7390ecdb992c70e86a6216
luizawestruppdocarmo <luizawestruppdocarmo@gmail.com>
2025-07-08 15:09:40 -0300

    FEAT: started creating 'principal #inicio' page informations

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <script setup>
+
 </script>
 
 <template>
-  <h1>oi</h1>
+  <header>
+    <a href="App.vue"><img src="/src/img/minilogo.png" alt=""></a>
+    <a href="#">Sobre</a>
+    <a href="#">Contato</a>
+  </header>
+  <main>
+    <section>
+      <img src="/src/img/logoroomie.png" alt="">
+      <h1>O espaço digital da sua república.</h1>
+      <p>Organização começa por aqui.</p>
+      <section>
+        <h2>Participe já!</h2>
+        <button>Cadastrar</button>
+        <button>Entrar</button>
+      </section>
+    </section>
+  </main>
 </template>
 
 <style scoped>
+template {
+  background-color: yellowgreen;
+}
 </style>
