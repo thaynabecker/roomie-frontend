ccde00ac744abd5c25d364e7b3afba6435d6218b
juliacdss <juliacostaifc@gmail.com>
2025-08-07 08:43:54 -0300

    FEAT: create profile page layout #28

--- src/views/PerfilMoradorView.vue
+++ src/views/PerfilMoradorView.vue
@@ -1,10 +1,9 @@
 <script setup>
 import icon from '@/assets/img/icon.png'
-<<<<<<< HEAD
-=======
 import SideBar from '@/components/SideBar.vue'
 import FooterComponent from '@/components/FooterComponent.vue'
->>>>>>> ae90eb3 (FEAT: create profile page layout  #28)
+//import SideBar from '@/components/SideBar.vue'
+//import FooterComponent from '@/components/FooterComponent.vue'
 
 // Simulando dados do banco:
 const usuario = {
@@ -48,7 +47,6 @@ const usuario = {
 </template>
 
 <style scoped>
-<<<<<<< HEAD
 .main {
     display: flex;
     text-align: center;
@@ -134,50 +132,46 @@ html {
 .infos {
     display: flex;
     flex-direction: column;
-    gap: 16px;
-=======
+    gap: 16px;}
 html {
     font-family: 'Poppins', sans-serif;;
 }
 .main {
     display: flex;
+    text-align: center;
 }
 .perfil {
-  padding: 40px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  color: #000;
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    color: #000;
 }
 .textos {
-  margin-bottom: 20px;
+    margin: 0 0 20px 0;
 }
 .textos h1 {
-  font-size: 28px;
-  margin: 0;
+    font-size: xx-large;
+    font-weight: bold;
 }
 .textos p {
-  font-size: 14px;
-  color: #666;
-  margin-top: 4px;
+    font-size: 14px;
+    color: #666;
 }
 .conteudo {
-  display: flex;
-  align-items: center;
-  gap: 60px;
+    display: flex;
+    align-items: center;
+    gap: 60px;
 }
 .foto-container img {
-  width: 180px;
-  height: 180px;
-  border: 5px solid black;
-  border-radius: 50%;
-  padding: 16px;
+    width: 300px;
+    height: 300px;
+    padding: 16px;
 }
 .infos {
   display: flex;
   flex-direction: column;
   gap: 16px;
->>>>>>> ae90eb3 (FEAT: create profile page layout  #28)
 }
 .campo {
   background-color: #7A0C0C;
@@ -186,7 +180,11 @@ html {
   border-radius: 12px;
   font-weight: bold;
   min-width: 280px;
-<<<<<<< HEAD
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+}
+.campo {
     font-size: 20px;
     background-color: #7A0C0C;
     color: white;
@@ -194,7 +192,5 @@ html {
     border-radius: 12px;
     font-weight: bold;
     min-width: 400px;
-=======
->>>>>>> ae90eb3 (FEAT: create profile page layout  #28)
 }
 </style>
