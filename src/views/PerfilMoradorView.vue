0014bfe845455c7934624b80df35a1f35ead3244
juliacdss <juliacostaifc@gmail.com>
2025-08-07 08:43:54 -0300

    FEAT: create profile page layout #28

--- src/views/PerfilMoradorView.vue
+++ src/views/PerfilMoradorView.vue
@@ -1,12 +1,5 @@
 <script setup>
 import icon from '@/assets/img/icon.png'
-<<<<<<< HEAD
-//import SideBar from '@/components/SideBar.vue'
-//import FooterComponent from '@/components/FooterComponent.vue'
-=======
-import SideBar from '@/components/SideBar.vue'
-import FooterComponent from '@/components/FooterComponent.vue'
->>>>>>> 3def049 (FEAT: create profile page layout  #28)
 
 // Simulando dados do banco:
 const usuario = {
@@ -50,7 +43,6 @@ const usuario = {
 </template>
 
 <style scoped>
-<<<<<<< HEAD
 .main {
     display: flex;
     text-align: center;
@@ -96,48 +88,47 @@ const usuario = {
     border-radius: 12px;
     font-weight: bold;
     min-width: 400px;
-=======
+}
 html {
     font-family: 'Poppins', sans-serif;;
 }
+
 .main {
     display: flex;
+    text-align: center;
 }
 .perfil {
-  padding: 40px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  color: #000;
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    color: #000;
 }
 .textos {
-  margin-bottom: 20px;
+    margin: 0 0 20px 0;
 }
 .textos h1 {
-  font-size: 28px;
-  margin: 0;
+    font-size: xx-large;
+    font-weight: bold;
 }
 .textos p {
-  font-size: 14px;
-  color: #666;
-  margin-top: 4px;
+    font-size: 14px;
+    color: #666;
 }
 .conteudo {
-  display: flex;
-  align-items: center;
-  gap: 60px;
+    display: flex;
+    align-items: center;
+    gap: 60px;
 }
 .foto-container img {
-  width: 180px;
-  height: 180px;
-  border: 5px solid black;
-  border-radius: 50%;
-  padding: 16px;
+    width: 300px;
+    height: 300px;
+    padding: 16px;
 }
 .infos {
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
 }
 .campo {
   background-color: #7A0C0C;
@@ -146,6 +137,12 @@ html {
   border-radius: 12px;
   font-weight: bold;
   min-width: 280px;
->>>>>>> 3def049 (FEAT: create profile page layout  #28)
+    font-size: 20px;
+    background-color: #7A0C0C;
+    color: white;
+    padding: 12px 20px;
+    border-radius: 12px;
+    font-weight: bold;
+    min-width: 400px;
 }
 </style>
