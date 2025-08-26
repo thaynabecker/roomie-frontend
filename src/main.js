79f1500dc079ab1c771b97833102811087cac8cb
Julia Costa da Silva <161061370+juliacdss@users.noreply.github.com>
2025-08-26 14:19:09 -0300

    Revert "FEAT-1: InicioView.vue CSS"

--- src/main.js
+++ src/main.js
@@ -1,18 +1,6 @@
 import './assets/main.css'
 
-import '@mdi/font/css/materialdesignicons.min.css'
-
-import { createApp } from 'vue';
-import { createPinia } from 'pinia'
-
-import router from './router'
+import { createApp } from 'vue'
 import App from './App.vue'
 
-const pinia = createPinia()
-
-const app = createApp(App)
-app.use(pinia)
-app.use(router)
-app.mount('#app')
-
-// createApp(App).use(router).mount('#app')
+createApp(App).mount('#app')
