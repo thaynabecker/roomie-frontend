9240e8684abe5d6a3679b8d80e43a1ce687c19c8
annaaraujoifc <annaluizaaraujo.ifcaraquari@gmail.com>
2025-09-16 08:39:06 -0300

    FEAT(sidebar): integrate router-link while keeping layout intact

--- src/components/SideBarComponent.vue
+++ src/components/SideBarComponent.vue
@@ -6,17 +6,48 @@
 
     <nav>
       <ul>
-        <li><span class="material-symbols-outlined">home</span><span>Início</span></li>
-        <li><span class="material-symbols-outlined">payments</span><span>Despesas</span></li>
-        <li><span class="material-symbols-outlined">business</span><span>Departamento</span></li>
-        <li><span class="material-symbols-outlined">sms</span><span>Mural</span></li>
-        <li><span class="material-symbols-outlined">account_box</span><span>Moradores</span></li>
-        <li><span class="material-symbols-outlined">info</span><span>Sobre</span></li>
+        <li>
+          <router-link to="/">
+            <span class="material-symbols-outlined">home</span>
+            <span>Início</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/despesas">
+            <span class="material-symbols-outlined">payments</span>
+            <span>Despesas</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/departamentos">
+            <span class="material-symbols-outlined">business</span>
+            <span>Departamento</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/mural">
+            <span class="material-symbols-outlined">sms</span>
+            <span>Mural</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/moradores">
+            <span class="material-symbols-outlined">account_box</span>
+            <span>Moradores</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/sobre">
+            <span class="material-symbols-outlined">info</span>
+            <span>Sobre</span>
+          </router-link>
+        </li>
       </ul>
     </nav>
 
     <button class="logout">
-      <span class="material-symbols-outlined">logout</span>Sair
+      <span class="material-symbols-outlined">logout</span>
+      Sair
     </button>
   </div>
 </template>
@@ -62,25 +93,27 @@ nav ul {
   margin:0;
 }
 nav li {
+  padding: 0.8rem 0;
+}
+nav li a {
   display: flex;
   align-items: center;
   gap: 12px;
-  padding: 0.8rem 0;
-  cursor: pointer;
+  color: white;
+  text-decoration: none;
   font-weight: 500;
 }
-nav li span:first-child {
+nav li a span:first-child {
   font-size: 24px;
   font-weight: 500;
 }
-nav li span:last-child {
+nav li a span:last-child {
   flex: 1;
   text-align: left;
 }
 
 /* Hover */
-nav li:hover {
-  color: #fff;
+nav li a:hover {
   opacity: 0.9;
 }
 
