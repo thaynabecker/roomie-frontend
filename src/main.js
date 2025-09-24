9a0c444acc0667ff4db9b1e838360c54aa96d825
juliacdss <juliacostaifc@gmail.com>
2025-09-23 22:11:36 -0300

    Resolve merge conflict no main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import './assets/main.css'
-<<<<<<< HEAD
-<<<<<<< HEAD
 import '@mdi/font/css/materialdesignicons.min.css'
 
 import { createApp } from 'vue'
@@ -15,17 +13,3 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
-=======
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
->>>>>>> 79f1500 (Revert "FEAT-1: InicioView.vue CSS")
-=======
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
->>>>>>> f055b97419aad3308f793723763d211334b31b0e
